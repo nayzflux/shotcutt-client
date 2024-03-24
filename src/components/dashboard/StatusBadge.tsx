c202e692e68b7c4b00679055c55eb2842936d190
nayzflux <55506519+nayzflux@users.noreply.github.com>
2024-03-24 16:31:45 +0100

    fix: waiting badge

--- src/components/dashboard/StatusBadge.tsx
+++ src/components/dashboard/StatusBadge.tsx
@@ -11,8 +11,8 @@ const StatusBadge = ({
             className={cn(
                 "p-1  bg-opacity-30 text-xs rounded",
                 status === "PROCESSED" && "bg-green-600 text-green-500",
-                status === "WAITING" ||
-                    (status === "PROCESSING" && "bg-blue-600 text-blue-500"),
+                (status === "WAITING" || status === "PROCESSING") &&
+                    "bg-blue-600 text-blue-500",
                 status === "FAILED" && "bg-red-600 text-red-500",
                 status === "UPLOADING" && "bg-yellow-500 text-yellow-400"
             )}
